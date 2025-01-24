295863f3334bd875e0435b6e50e94202ee7de00d
Brendan Walker <brendan@millerwalker.net>
2025-01-24 21:34:23 +0100

    Stubbing in game camera

--- ggj-2025-unity/Assets/Game/Scripts/GameController.cs
+++ ggj-2025-unity/Assets/Game/Scripts/GameController.cs
@@ -1,13 +1,18 @@
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class GameController : Singleton<GameController>
 {
     [SerializeField]
     private LevelGenerator _levelManager;
 
+    [SerializeField]
+    private LevelCameraController _cameraController;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        MainCamera.Instance.CameraStack.PushController(_cameraController);
+
         _levelManager.GenerateLevel();
     }
 
