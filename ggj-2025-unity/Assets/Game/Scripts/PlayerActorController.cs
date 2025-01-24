c8b571f8947ebb041b40617135802e521657ed51
David Evans <david0evans@gmail.com>
2025-01-24 22:09:03 +0100

    pick up gum balls

--- ggj-2025-unity/Assets/Game/Scripts/PlayerActorController.cs
+++ ggj-2025-unity/Assets/Game/Scripts/PlayerActorController.cs
@@ -1,15 +1,27 @@
+using System;
 using UnityEngine;
 
 public class PlayerActorController : MonoBehaviour
 {
   [SerializeField] private ActorController _actor = null;
   [SerializeField] private PlayerAnimation _playerAnimation = null;
+  [SerializeField] private InteractionController _interaction = null;
 
   private Rewired.Player _playerInput;
 
   private void Awake()
   {
     _playerInput = Rewired.ReInput.players.GetPlayer(0);
+  }
+
+  private void OnEnable()
+  {
+    _interaction.Interacted += OnInteracted;
+  }
+
+  private void OnDisable()
+  {
+    _interaction.Interacted -= OnInteracted;
   }
 
   private void Update()
@@ -23,6 +35,21 @@ public class PlayerActorController : MonoBehaviour
     if (inputJumpButton)
     {
       _actor.Jump();
+    }
+
+    if (inputInteractButton)
+    {
+      _interaction.TriggerInteract();
+    }
+  }
+
+  private void OnInteracted(InteractableObject interactable)
+  {
+    Item item = interactable.GetComponent<Item>();
+    if (item)
+    {
+      interactable.enabled = false;
+      _playerAnimation.HoldItem(item);
     }
   }
 }
--- ggj-2025-unity/Assets/Game/Scripts/PlayerAnimation.cs
+++ ggj-2025-unity/Assets/Game/Scripts/PlayerAnimation.cs
@@ -10,13 +10,33 @@ public class PlayerAnimation : MonoBehaviour
   [SerializeField] private Transform _mouthItemRoot = null;
 
   private float _biteAnimTimer;
+  private Item _mouthItem;
+
+  public void HoldItem(Item item)
+  {
+    _mouthItem = item;
+    item.transform.parent = _mouthItemRoot;
+  }
 
   private void Update()
   {
-    _biteAnimTimer += Time.deltaTime * BiteAnimSpeed;
+    float dt = Time.deltaTime;
 
-    float jawAngle = Mathf.Abs(Mathf.Sin(_biteAnimTimer)) * BiteAnimAngle;
-    _jawUpper.localRotation = Quaternion.Euler(-jawAngle, 0, 0);
-    _jawLower.localRotation = Quaternion.Euler(jawAngle, 0, 0);
+    if (_mouthItem)
+    {
+      _mouthItem.transform.localPosition = Mathfx.Damp(_mouthItem.transform.localPosition, Vector3.zero, 0.25f, dt * 5);
+
+      float jawAngle = 20;
+      _jawUpper.localRotation = Quaternion.Euler(-jawAngle, 0, 0);
+      _jawLower.localRotation = Quaternion.Euler(jawAngle, 0, 0);
+    }
+    else
+    {
+      _biteAnimTimer += dt * BiteAnimSpeed;
+
+      float jawAngle = Mathf.Abs(Mathf.Sin(_biteAnimTimer)) * BiteAnimAngle;
+      _jawUpper.localRotation = Quaternion.Euler(-jawAngle, 0, 0);
+      _jawLower.localRotation = Quaternion.Euler(jawAngle, 0, 0);
+    }
   }
 }
