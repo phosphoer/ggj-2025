50a2066ceddffad056520aae9971332bee9d93e4
Brendan Walker <brendan@millerwalker.net>
2025-01-24 20:27:28 +0100

    Add reference to the LevelManager in the GameManager

--- ggj-2025-unity/Assets/Game/Scripts/GameManager.cs
+++ ggj-2025-unity/Assets/Game/Scripts/GameManager.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]
+    private LevelManager _levelManager;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
