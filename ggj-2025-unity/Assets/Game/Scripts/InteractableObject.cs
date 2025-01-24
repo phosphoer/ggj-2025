30525ff842d3ac9954a2b3a0b465ba08d0eac8b0
David Evans <david0evans@gmail.com>
2025-01-24 21:41:41 +0100

    set up world ui and interactions

---
+++ ggj-2025-unity/Assets/Game/Scripts/InteractableObject.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class InteractableObject : MonoBehaviour
+{
+  public static IReadOnlyList<InteractableObject> Instances => _instances;
+
+  public event System.Action<InteractionController> Interacted;
+
+  public float InteractDistance = 100;
+  public BoolStateStack DisableInteraction = new();
+
+  [SerializeField] private InteractPromptUI _promptUIPrefab = null;
+  [SerializeField] private string _interactPromptText = "Pickup";
+
+  private RectTransform _promptUIRoot;
+
+  private static List<InteractableObject> _instances = new();
+
+  public void Interact(InteractionController interactionController)
+  {
+    Interacted?.Invoke(interactionController);
+  }
+
+  public void ShowPrompt()
+  {
+    HidePrompt();
+    _promptUIRoot = WorldUIManager.Instance.ShowItem(transform, Vector3.up);
+    InteractPromptUI uiPrompt = Instantiate(_promptUIPrefab, _promptUIRoot);
+    uiPrompt.PromptText = _interactPromptText;
+  }
+
+  public void HidePrompt()
+  {
+    if (_promptUIRoot != null)
+    {
+      WorldUIManager.Instance.HideItem(_promptUIRoot);
+      _promptUIRoot = null;
+    }
+  }
+
+  private void OnEnable()
+  {
+    _instances.Add(this);
+  }
+
+  private void OnDisable()
+  {
+    _instances.Remove(this);
+  }
+}
---
+++ ggj-2025-unity/Assets/Game/Scripts/InteractionController.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+
+public class InteractionController : MonoBehaviour
+{
+  public event System.Action<InteractableObject> Interacted;
+
+  private int _updateIndex;
+  private InteractableObject _currentInteractable;
+  private InteractableObject _closestInteractable;
+  private float _currentMinInteractableDist = Mathf.Infinity;
+
+  public void TriggerInteract()
+  {
+    if (_currentInteractable != null)
+    {
+      _currentInteractable.Interact(this);
+      Interacted?.Invoke(_currentInteractable);
+    }
+  }
+
+  private void Update()
+  {
+    // Check which interactables are in range, one per frame
+    if (_updateIndex < InteractableObject.Instances.Count)
+    {
+      InteractableObject interactable = InteractableObject.Instances[_updateIndex];
+
+      Vector3 toInteractable = interactable.transform.position - transform.position;
+      float distance = toInteractable.magnitude;
+      if (distance < interactable.InteractDistance && distance < _currentMinInteractableDist)
+      {
+        _closestInteractable = interactable;
+        _currentMinInteractableDist = distance;
+      }
+    }
+
+    // Evaluate what current interactable is once we've looked at them
+    _updateIndex = Mathfx.Wrap(_updateIndex + 1, 0, InteractableObject.Instances.Count);
+    if (_updateIndex == 0)
+    {
+      // Handle a change of current interactable
+      if (_closestInteractable != _currentInteractable)
+      {
+        // Unregister current interactable
+        if (_currentInteractable != null)
+        {
+          _currentInteractable.HidePrompt();
+        }
+
+        _currentInteractable = _closestInteractable;
+
+        // Register new interactable
+        if (_currentInteractable != null)
+        {
+          _currentInteractable.ShowPrompt();
+        }
+      }
+
+      // Reset closest interactable
+      _currentMinInteractableDist = Mathf.Infinity;
+      _closestInteractable = null;
+    }
+  }
+}
